be9a3a0f687eefb34fa7b124aca7a6633e716ed0
e.karpov <e.karpov@npo-echelon.ru>
2018-05-25 10:31:50 +0300

    params to decorator

--- src/dataclass.ts
+++ src/dataclass.ts
@@ -1,16 +1,26 @@
 import * as util from 'util';
 
-export default function dataclass<T extends {new(...args:any[]):{}}>(constructor: T) {
-  return class extends constructor {
-    /** Method for representation of object while usijng confole.log */
-    [util.inspect.custom](depth, options) {
-      return this.toString();
+export interface DataClassProps {
+  eq?: false;
+}
+
+export default function (props?: DataClassProps) { 
+  return function dataclass<T extends {new(...args:any[]):{}}>(constructor: T) {
+    const A = class extends constructor {
+      /** Method for representation of object while usijng confole.log */
+      [util.inspect.custom](depth, options) {
+        return this.toString();
+      }
+      /**
+       * String representation of the class;
+       */
+      toString() {
+        return `${constructor.name} [${Object.keys(this).map(key => `${key} := ${this[key]}`)}]`;
+      }
     }
-    /**
-     * String representation of the class;
-     */
-    toString() {
-      return `${constructor.name} [${Object.keys(this).map(key => `${key} := ${this[key]}`)}]`;
+    if (!props || props && props.eq) {
+      A.prototype.eq = (elem: T) => false;
     }
+    return A;
   }
 }
--- test/dataclass.test.ts
+++ test/dataclass.test.ts
@@ -1,13 +1,22 @@
 import dataclass from "../build/dataclass";
 import {suite, test} from "mocha-typescript";
 
-@dataclass class A {
+@dataclass() class A {
   some: string = "asdad";
+}
+
+@dataclass({eq: false }) class B {
+  next: string = "fdsfds";
 }
 
 @suite class DataClassTest {
   @test toStringOverride() {
     const a = new A();
     console.log(a);
+  }
+
+  @test toStringOverride2() {
+    const b = new B();
+    console.log(b);
   }
 }
